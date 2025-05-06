d8b2844da7e6fd1517979b290da8078ba7893471
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-05-06 09:47:13 -0700

    far closer to a working game
    added actual interaction for a dialog tree, and fixed teleportation regarding the camera

--- js/game-script.js
+++ js/game-script.js
@@ -33,9 +33,24 @@ let keyA
 let keyS
 let keyD
 let keyEnter
+let selectedOption = 1
+let canSelect = true
+let isInDialog = false
+let basementDoorUnlocked = false
+
+const option1 = document.getElementById("opt1");
+const option2 = document.getElementById("opt2");
+const option3 = document.getElementById("opt3");
+option1.classList.remove("highlight", "normal");
+option2.classList.remove("highlight", "normal");
+option3.classList.remove("highlight", "normal");
+option1.classList.add("highlight");
+option2.classList.add("normal");
+option3.classList.add("normal");
 
 // =====================================================================
 function preload() {
+    this.load.image("teleporter", "../media/inside-portal-machine.png");
     this.load.image("basement", "../media/basement.png");
     this.load.image("player", "../media/guy-standingT.png");
     this.load.image("alert", "../media/Alert!.png");
@@ -56,12 +71,13 @@ function create() {
 
     this.add.image(0, 0, "basement").setScale(2.5);
     this.add.image(2000, 43, "lab").setScale(2.5);
+    this.add.image(4000, 0, "teleporter").setScale(2.5);
     this.cursors = this.input.keyboard.createCursorKeys();
     cursors = this.cursors; // make cursors available globally
 
     this.player = this.physics.add.sprite(-100, 0, "player").setScale(2.2).setBounce(0).setCollideWorldBounds(false).setDepth(2);
     let stairs = this.physics.add.staticGroup();
-  
+
 
     function collider(object, scene, x, y, width, height) {
         let collider = scene.physics.add.staticSprite(x, y, "transparent");
@@ -106,7 +122,7 @@ function create() {
 }
 // =====================================================================
 
- let isInDialog = false
+
 
 // =====================================================================
 function update() {
@@ -117,23 +133,39 @@ function update() {
             this.player.scaleX = -2.2
             this.player.body.setOffset(20, 0)
         }
-    
+
         else if (cursors.right.isDown || keyD.isDown) {
             this.player.setVelocityX(160);
             this.player.scaleX = 2.2
             this.player.body.setOffset(0, 0)
         }
-    
+
         else {
             this.player.setVelocityX(0);
         }
-    
+
         if (cursors.up.isDown && this.player.body.touching.down || keyW.isDown && this.player.body.touching.down) {
             this.player.setVelocityY(-250);
         }
     }
 
+    if (isInDialog) {
+        this.player.setVelocityX(0);
+        // this.player.body.setOffset(0, 0)
+    }
 
+    const cameraCenterX = this.cameras.main.scrollX + this.cameras.main.width / 2;
+    console.log(cameraCenterX)
+    function teleportPlayer(scene, x, y) {
+        scene.cameras.main.setLerp(1, 1); // Adjust the values for smoother or faster movement
+        scene.player.x = x
+        scene.player.y = y
+    }
+
+    if (this.player.x == cameraCenterX) {
+        this.cameras.main.setLerp(0.01, 0.1);
+
+    }
 
     if (this.alerts == false || this.alerts == undefined) {
         this.alerts = {};
@@ -165,24 +197,99 @@ function update() {
     }
 
     playerNearAlert(this.player, this, -430, -100, () => {
-        console.log("Interacted with target location!"); 
-        this.player.x = 2100
-        this.player.y = 0
+        console.log("Interacted with target location!");
+        if (basementDoorUnlocked) {
+            teleportPlayer(this, 2000, 0) // teleport to lab
+            // this.player.x = 2100
+            // this.player.y = 0
+        }
+
+    });
+
+    playerNearAlert(this.player, this, 2240, 50, () => {
+        console.log("Interacted with target location!");
+        teleportPlayer(this, 4000, 0) // teleport to basement
+
     });
 
     playerNearAlert(this.player, this, 150, 50, () => {
-        console.log("Interacted with target location!"); 
+        console.log("Interacted with target location! aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         document.getElementById("onscreenText").style.display = "flex";
         isInDialog = true;
+        if (canSelect) {
+            if (selectedOption == 1) {
+                console.log("Option 1 selected");
+                // exitDialog()
+            } else if (selectedOption == 2) {
+                console.log("Option 2 selected");
+                // exitDialog()
+            } else if (selectedOption == 3) {
+                console.log("Option 3 selected");
+                basementDoorUnlocked = true
+                exitDialog()
+            } else {
+                console.log("No option selected");
+                // exitDialog()
+            }
+            setTimeout(() => {
+                canSelect = true
+            }, 150);
+        }
     });
 
-    if (keyEnter.isDown && isInDialog) {;
+    function selectingOption(upOrDown) {
+        if (canSelect) {
+            canSelect = false
+            selectedOption += upOrDown
+            if (selectedOption <= 0) {
+                selectedOption = 3
+            } else if (selectedOption >= 4) {
+                selectedOption = 1
+            }
+            option1.classList.remove("highlight", "normal");
+            option2.classList.remove("highlight", "normal");
+            option3.classList.remove("highlight", "normal");
+            if (selectedOption == 1) {
+                option1.classList.toggle("highlight");
+                option2.classList.toggle("normal");
+                option3.classList.toggle("normal");
+                console.log("Option 1 selected");
+            } else if (selectedOption == 2) {
+                option1.classList.toggle("normal");
+                option2.classList.toggle("highlight");
+                option3.classList.toggle("normal");
+                console.log("Option 2 selected");
+            } else if (selectedOption == 3) {
+                option1.classList.toggle("normal");
+                option2.classList.toggle("normal");
+                option3.classList.toggle("highlight");
+                console.log("Option 3 selected");
+            }
+            setTimeout(() => {
+                canSelect = true
+            }, 150);
+        }
+        return selectedOption
+    }
+
+    if (isInDialog) {
+        if (cursors.right.isDown || keyD.isDown) {
+            selectingOption(1)
+        } else if (cursors.left.isDown || keyA.isDown) {
+            selectingOption(-1)
+        }
+    }
+
+    function exitDialog() {
         document.getElementById("onscreenText").style.display = "none";
         isInDialog = false;
         console.log("Dialog closed");
     }
 
-  
+    if (keyEnter.isDown && isInDialog) {
+        exitDialog()
+        // this is the emergency exit
+    }
 
 }
 // =====================================================================
